8d6abefff3c7ab65876585509ea9e92fc2479b64
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-18 20:14:51 +0200

    Data types

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,14 +1,22 @@
+DROP TABLE users;
+
 CREATE TABLE users(
     first_name varchar(64),
     last_name varchar(64),
-    email text
+    email text,
+    gender varchar(30),
+    is_subscribe boolean,
+    birthday date,
+    foot_size smallint
+    height numeric(3, 2)
 );
 
 /*
 
-char(5): '1234567' -> error, '12345' -> '12345', '12' -> '12   '
-varchar(5): '1234567' -> error, '12345' -> '12345', '12' -> '12'
+1.75
 
-text -> max 1GB
+numeric(точность, масштаб)
+точность - 3
+масштаб - 2
 
 */
