e0724f77f0a78104318d89f0661bf33b97ee4b2f
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-18 20:36:30 +0200

    Inserting data into a table

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -7,16 +7,12 @@ CREATE TABLE users(
     gender varchar(30),
     is_subscribe boolean,
     birthday date,
-    foot_size smallint
+    foot_size smallint,
     height numeric(3, 2)
 );
 
-/*
 
-1.75
-
-numeric(точность, масштаб)
-точность - 3
-масштаб - 2
-
-*/
+INSERT INTO users VALUES
+('Petr','Petrov','mail','male',true,'1970-09-15',49,2.10),
+('Sidr','Sidorov','mail','male',true,'1971-09-15',44,1.92),
+('','','mail','male',true,'1970-09-15',49,2.10);
