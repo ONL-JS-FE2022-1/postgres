3e87188aa82bdfb904f60e5b4df28e440d36e47c
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-18 20:04:28 +0200

    Creating table

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,1 +1,14 @@
-CREATE TABLE test();
+CREATE TABLE users(
+    first_name varchar(64),
+    last_name varchar(64),
+    email text
+);
+
+/*
+
+char(5): '1234567' -> error, '12345' -> '12345', '12' -> '12   '
+varchar(5): '1234567' -> error, '12345' -> '12345', '12' -> '12'
+
+text -> max 1GB
+
+*/
