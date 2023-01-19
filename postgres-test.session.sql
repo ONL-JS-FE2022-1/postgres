97cb86224d8ad15bec6d753a2e0dd5950383825a
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-19 19:35:17 +0200

    Primary key

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,28 +1,23 @@
-DROP TABLE users;
+DROP TABLE messages;
 
-CREATE TABLE users(
-    first_name varchar(64) NOT NULL CHECK(first_name != ''),
-    last_name varchar(64) NOT NULL CONSTRAINT last_name_not_empty CHECK(last_name != ''),
-    email text NOT NULL CHECK(email != '') UNIQUE,
-    gender varchar(30),
-    is_subscribe boolean NOT NULL,
-    birthday date CHECK(birthday < current_date),
-    foot_size smallint,
-    height numeric(3, 2) CONSTRAINT too_high_user CHECK(height < 3.0)
+CREATE TABLE messages(
+    id serial PRIMARY KEY,
+    body text NOT NULL CHECK (body != ''),
+    author varchar(256) NOT NULL CHECK (author != ''),
+    created_at timestamp DEFAULT current_timestamp,
+    is_read boolean DEFAULT false
 );
 
+INSERT INTO messages VALUES (
+    'Hello world',
+    'Me'
+);
 
-INSERT INTO users VALUES
-(NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO messages (author, body) VALUES 
+('Me', 'Hello again'),
+('Me', 'And again'),
+('Me', 'And again');
 
-INSERT INTO users VALUES
-('Petr','Petrov','mail122','male',true,'1970-09-15',NULL,2.10),
-('Sidr','Sidorov','mail2312','male',true,'1971-09-15',44,1.92),
-('Ivanov','sfsfsdfsf','mail3','male2132132',true,'1970-09-15',NULL,2.10);
-
-
-INSERT INTO users VALUES
-('John','Doe','johndoe@gmail.com','male',false,'2023-01-21',48,1.85);
-
-
-// CONSTRAINT
+INSERT INTO messages VALUES (
+    2, 'Text', 'Author'
+);
