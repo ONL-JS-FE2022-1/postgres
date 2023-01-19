f2ff290da74042018609d5d064148f0198204d76
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-19 18:39:22 +0200

    Constraints (1st)

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,14 +1,14 @@
 DROP TABLE users;
 
 CREATE TABLE users(
-    first_name varchar(64) NOT NULL,
-    last_name varchar(64) NOT NULL,
-    email text NOT NULL,
+    first_name varchar(64) NOT NULL CHECK(first_name != ''),
+    last_name varchar(64) NOT NULL CONSTRAINT last_name_not_empty CHECK(last_name != ''),
+    email text NOT NULL CHECK(email != '') UNIQUE,
     gender varchar(30),
     is_subscribe boolean NOT NULL,
     birthday date,
     foot_size smallint,
-    height numeric(3, 2)
+    height numeric(3, 2) CONSTRAINT too_high_user CHECK(height < 3.0)
 );
 
 
@@ -16,6 +16,13 @@ INSERT INTO users VALUES
 (NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
 INSERT INTO users VALUES
-('Petr','Petrov','mail','male',true,'1970-09-15',NULL,2.10),
-('Sidr','Sidorov','mail','male',true,'1971-09-15',44,1.92),
-('Ivanov','','mail','male',true,'1970-09-15',NULL,2.10);
+('Petr','Petrov','mail122','male',true,'1970-09-15',NULL,2.10),
+('Sidr','Sidorov','mail2312','male',true,'1971-09-15',44,1.92),
+('Ivanov','sfsfsdfsf','mail3','male2132132',true,'1970-09-15',NULL,2.10);
+
+
+INSERT INTO users VALUES
+('Petr','Petrov','mail23','male',true,'1970-09-15',NULL,2.10);
+
+
+// CONSTRAINT
