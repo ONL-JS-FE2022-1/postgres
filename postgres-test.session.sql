9ce16ffa2873a5c996f42cbefbacf2e99e6a851a
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-20 20:26:36 +0200

    Add insert

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -15,3 +15,10 @@ CREATE TABLE orders_to_products (
     quantity int,
     PRIMARY KEY(order_id, product_id)
 );
+
+INSERT INTO orders (customer_id) VALUES(4);
+
+INSERT INTO orders_to_products (order_id, product_id, quantity) VALUES
+(1, 1, 2),
+(1, 3, 1),
+(1, 4, 1);
