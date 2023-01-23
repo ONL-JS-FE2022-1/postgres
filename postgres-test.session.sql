c90d62f55183a4ec9de70e1556c1ef790fe7b511
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-23 19:44:41 +0200

    Update, returning

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,13 +1,31 @@
-CREATE TABLE coaches(
+CREATE TABLE contents(
     id serial PRIMARY KEY,
-    name varchar(256) NOT NULL
+    name varchar(256) NOT NULL,
+    description text,
+    author_id int REFERENCES users(id),
+    created_at timestamp DEFAULT current_timestamp
 );
 
-CREATE TABLE teams(
-    id serial PRIMARY KEY,
-    name varchar(256),
-    coach_id int REFERENCES coaches(id)
+CREATE TABLE reactions (
+    content_id int REFERENCES contents(id) ON DELETE CASCADE,
+    user_id int REFERENCES users(id),
+    is_liked boolean
 );
 
-ALTER TABLE coaches
-ADD COLUMN team_id int REFERENCES teams(id);
+INSERT INTO contents (name, author_id) VALUES
+('funny dogs', 2);
+
+INSERT INTO contents (name, author_id) VALUES
+('1984', 1)
+RETURNING id;
+
+UPDATE contents SET name = 'funny cats' WHERE id = 2;
+UPDATE contents SET author_id = 4 WHERE id = 2;
+
+INSERT INTO reactions VALUES(
+    1, 1, false
+);
+
+DROP TABLE reactions;
+
+DELETE FROM contents WHERE id = 1;
