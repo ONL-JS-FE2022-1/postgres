fa3c8a5f5063b10b268930d7f8067582d60fa0f2
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-01-27 18:31:30 +0200

    Aliases

--- select.sql
+++ select.sql
@@ -1,59 +1,10 @@
+SELECT *, extract('years'from age(birthday)) AS years 
+FROM users
+WHERE extract('years'from age(birthday)) BETWEEN 20 AND 40;
 
-/*
+SELECT first_name AS "Ім'я", 
+last_name AS "Прізвище", 
+id AS "Особистий номер" 
+FROM users;
 
-1. Отримати всіх повнолітніх користувачів чоловічої статі.
-
-2. Отримати всіх користувачів-жінок, ім'я яких починається на "А".
-
-3. Отримати всіх користувачів, вік яких від 20 до 40 років.
-
-4. Отримати всіх користувачів, які народились у вересні.
-
-5. Всім користувачам, які народились 1 листопада, змінити підписку на true.
-
-6. Видалити всіх користувачів, які старші за 65 років.
-
-7. Всім користувачам чоловічого роду віком 40 до 50 років встановити вагу = 80.
-
-*/
-
---1
-
-SELECT first_name, last_name, gender, birthday, extract('years' from age(birthday)) FROM users
-WHERE gender = 'male' AND extract('years' from age(birthday)) >= 18;
-
-
---2
-SELECT * FROM users
-WHERE gender = 'female' AND first_name LIKE 'A%';
-
---3
-SELECT first_name, last_name, birthday, extract('years' from age(birthday)) FROM users
-WHERE extract('years' from age(birthday)) BETWEEN 20 AND 40;
-
---4
-SELECT * FROM users
-WHERE extract('month' from birthday) = 9;
-
---5
-UPDATE users
-SET is_subscribe = true
-WHERE extract('month' from birthday) = 11 AND extract('day' from birthday) = 1;
-
-SELECT * FROM users
-WHERE extract('month' from birthday) = 11 AND extract('day' from birthday) = 1;
-
---6
-DELETE FROM users
-WHERE extract('years' from age(birthday)) > 65;
-
-SELECT first_name, last_name, gender, birthday, extract('years' from age(birthday)) FROM users
-WHERE extract('years' from age(birthday)) > 65;
-
---7
-UPDATE users
-SET weight = 80
-WHERE gender = 'male' AND extract('years' from age(birthday)) BETWEEN 40 AND 50;
-
-SELECT first_name, gender, birthday, extract('years' from age(birthday)) FROM users
-WHERE gender = 'male' AND extract('years' from age(birthday)) BETWEEN 40 AND 50;
+SELECT * FROM orders_to_products AS "Чек";
