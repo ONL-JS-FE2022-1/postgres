3ffc683bfecda2f610f08aca571d3e615c343d5e
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-03 18:49:43 +0200

    Home practice task resolve

--- normalize.sql
+++ normalize.sql
@@ -146,3 +146,82 @@ CREATE TABLE pizzas_to_restaurants(
     restaurant_id int REFERENCES restaurants(id),
     PRIMARY KEY (pizza_type, restaurant_id)
 );
+
+
+/*
+
+Необхідно спроектувати БД поставки товарів.
+В БД має зберігатися наступна інформація:
+
+ТОВАРИ: 
+- код товара, 
+- назва, 
+- ціна 
+
+ЗАМОВЛЕННЯ НА ПОСТАВКУ: 
+- код замовлення, 
+- найменування постачальника, 
+- адреса постачальника, 
+- телефон, 
+- номер догоовору, 
+- найменування товару, 
+- план поставки (кількість в шт.)
+
+ФАКТИЧНІ ВІДГРУЗКИ ТОВАРУ: 
+- код відгрузки, 
+- код замовлення, 
+- дата відгрузки, 
+- відгружено товару (шт)
+
+При проектуванні БД необхідно врахувати:
+- товар має декілька замовлень на постачання
+- замовлення відповідає одному товару
+- товару можуть відповідати декілька відгрузок
+- у одній відгрузці може бути декілька товарів
+
+- товар не обов'язково має замовлення. Кожне замовлення відповідає товару
+- товар не обов'язково відгружається замовнику, але кожна відгрузка відповідає певному товару
+
+//
+ПЛАН ПОСТАВКИ: у вас може бути замовлення, але ви його можете виконати не одразу, а частинами
+
+*/
+
+
+CREATE TABLE products(
+    id serial PRIMARY KEY,
+    name varchar(300) CHECK (name != '') NOT NULL
+);
+
+CREATE TABLE manufacturers(
+    id serial PRIMARY KEY,
+    name varchar(300) CHECK (name != '') NOT NULL,
+    address text NOT NULL,
+    tel_number varchar(20) NOT NULL 
+);
+
+CREATE TABLE orders(
+    id serial PRIMARY KEY,
+    product_id int REFERENCES products(id),
+    quantity_plan int NOT NULL,
+    contract_number int NOT NULL,
+    contract_date date NOT NULL,
+    manufacturer_id int REFERENCES manufacturers(id),
+    cost decimal(10,2) NOT NULL
+);
+
+CREATE TABLE shipments(
+    id serial PRIMARY KEY,
+    order_id int REFERENCES orders(id),
+    shipment_date date NOT NULL
+);
+
+CREATE TABLE products_to_shipments(
+    product_id REFERENCES products(id),
+    shipment_id REFERENCES shipments(id),
+    product_quantity int NOT NULL,
+    PRIMARY KEY(product_id, shipment_id)
+);
+
+
+
