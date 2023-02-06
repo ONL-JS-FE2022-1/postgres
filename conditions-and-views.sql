eb5345df5a217aaef68f665794e55e1e9f93eb6a
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-06 20:01:33 +0200

    Views

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,92 +1,31 @@
---------Підзапити----
-
-----IN, NOT IN, SOME, ANY, EXISTS, ALL
+-----VIEWS----
 
 
-SELECT * FROM users AS u
-WHERE u.id NOT IN (
-    SELECT customer_id FROM orders
-);
-
-
----Знайти телефони, які ніхто не замовляв
-
-SELECT * FROM products AS p
-WHERE p.id NOT IN (
-    SELECT product_id FROM orders_to_products
-);
-
-------EXISTS
-
-
-SELECT EXISTS 
-(SELECT * FROM users
-WHERE id = 23564);
-
-
-SELECT *
+SELECT u.*, count(o.id) AS "orders_amount"
 FROM users AS u
-WHERE EXISTS
-(SELECT * FROM orders AS o
-WHERE u.id = o.customer_id
+LEFT JOIN orders AS o
+ON u.id = o.customer_id
+GROUP BY u.id, u.email
+ORDER BY "orders_amount";
+
+
+CREATE OR REPLACE VIEW users_with_orders_amount AS (
+    SELECT u.*, count(o.id) AS "orders_amount"
+    FROM users AS u
+    LEFT JOIN orders AS o
+    ON u.id = o.customer_id
+    GROUP BY u.id, u.email
+    ORDER BY "orders_amount"
 );
 
+DROP VIEW users_with_orders_amount;
 
 
-SELECT EXISTS
-(SELECT * FROM orders AS o
-WHERE o.customer_id = 546);
-
-
-------SOME/ANY
-
-
-SELECT * FROM users AS u
-WHERE u.id = ANY 
-(SELECT customer_id FROM orders);
-
-SELECT * FROM users AS u
-WHERE u.id = SOME 
-(SELECT customer_id FROM orders);
-
-SELECT * FROM users AS u
-WHERE u.id IN (
-    SELECT customer_id FROM orders
-);
-
-
-------ALL
-
-SELECT * FROM products AS p
-WHERE p.id != ALL
-(SELECT product_id FROM orders_to_products);
+SELECT * FROM users_with_orders_amount;
 
 
 
 
-
-
-
-
-
------Знайти всі телефони, які купував юзер з id 23
-
-
-SELECT * FROM products AS p
-WHERE p.id = ANY
-(SELECT product_id FROM orders_to_products AS otp
-WHERE order_id = SOME
-    (
-        SELECT id FROM orders AS o
-        WHERE customer_id = 23
-    )
-);
-
-
-
-SELECT * FROM products AS p
-JOIN orders_to_products AS otp
-ON otp.product_id = p.id
-JOIN orders AS o
-ON otp.order_id = o.id
-WHERE o.customer_id = 23;
+---------Витягти всі мейли юзерів, які мають замовлення
+SELECT email FROM users_with_orders_amount
+WHERE orders_amount = 1;
