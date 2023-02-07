d1c2aa5cb72b99c53aeb0632744ca76238742d4a
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-07 18:48:38 +0200

    ENUM and CAST

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -1,82 +1,42 @@
------VIEWS----
+/* ENUM */
+
+----- order status:
+--- true - виконано
+--- false - не виконано
 
 
-SELECT u.*, count(o.id) AS "orders_amount"
-FROM users AS u
-LEFT JOIN orders AS o
-ON u.id = o.customer_id
-GROUP BY u.id, u.email
-ORDER BY "orders_amount";
+--- processing -- procesg -- procesing
 
+-----('new', 'processing', 'shiped', 'done')
 
-CREATE OR REPLACE VIEW users_with_orders_amount AS (
-    SELECT u.*, count(o.id) AS "orders_amount"
-    FROM users AS u
-    LEFT JOIN orders AS o
-    ON u.id = o.customer_id
-    GROUP BY u.id, u.email
-    ORDER BY "orders_amount"
-);
-
+DROP VIEW users_with_total_amounts;
 DROP VIEW users_with_orders_amount;
+DROP VIEW orders_with_price;
 
+SELECT * FROM orders
+WHERE status = true;
 
-SELECT * FROM users_with_orders_amount;
+CREATE TYPE order_status AS ENUM('new', 'processing', 'shiped', 'done');
 
+ALTER TABLE orders
+ALTER COLUMN status TYPE order_status; --- problem!
 
+ALTER TABLE orders
+ALTER COLUMN status TYPE order_status
+USING(
+    CASE status
+    WHEN false THEN 'processing'
+    WHEN true THEN 'done'
+    ELSE 'new'
+    END
+)::order_status;
 
+INSERT INTO orders(customer_id, status) VALUES
+(4, 'new');
 
----------Витягти всі мейли юзерів, які мають замовлення
-SELECT email FROM users_with_orders_amount
-WHERE orders_amount = 1;
+SELECT * FROM orders
+WHERE id = 14;
 
-
-
-
-
-------Представлення, яке зберігає замовлення з їхньою вартістю
-
-CREATE VIEW orders_with_price AS (
-    SELECT o.id, o.customer_id, sum(p.price * otp.quantity) AS "order_sum", o.status
-    FROM orders AS o
-    JOIN orders_to_products AS otp
-    ON o.id = otp.order_id
-    JOIN products AS p
-    ON p.id = otp.product_id
-    GROUP BY o.id
-);
-
-
-SELECT * FROM orders_with_price;
-
-
-
---------Вивести юзерів з сумою коштів, які вони витратили в нашому магазині
-SELECT u.id, u.email, sum(owp.order_sum) AS sum_amount
-FROM users AS u
-JOIN orders_with_price AS owp
-ON u.id = owp.customer_id
-GROUP BY u.id
-ORDER BY sum_amount DESC
-LIMIT 10;
-
-
-
-
-
-
-
-
-
-
-CREATE VIEW users_with_total_amounts AS (
-    SELECT u.*, sum(owp.order_sum) AS "total_amount"
-    FROM users AS u
-    JOIN orders_with_price AS owp
-    ON u.id = owp.customer_id
-    GROUP BY u.id
-);
-
-SELECT * FROM users_with_total_amounts
-ORDER BY total_amount DESC
-LIMIT 10;
+UPDATE orders
+SET status = 'shiped'
+WHERE id = 14;
